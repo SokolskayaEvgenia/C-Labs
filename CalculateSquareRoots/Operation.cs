c145888fcf6de06afd32227c110c3ff38a0f66be
Евгения Сокольская <73002756+SokolskayaEvgenia@users.noreply.github.com>
2020-10-27 01:25:39 +0300

    контрольное упражнение 4.2

---
+++ CalculateSquareRoots/Operation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateSquareRoots
+{
+    class Operation
+    {
+        public static double CalculateSquareRoots (double a, double b, double c, ref double x1, ref double x2)
+        // public static double CalculateSquareRoots (double a, double b, double c, ref double x1, ref double x2)
+        {
+            double D = b * b - 4 * a * c; //вычисление дискриминанта
+            if (D>0)
+            {
+                x1 = (-b + Math.Sqrt(D)) / 2 * a;
+                x2 = (-b - Math.Sqrt(D)) / 2 * a;
+                return 1;
+            }
+            else if (D == 0)
+            {
+                x1 = -b / 2 * a;
+                //x2 = x1; //если возвращать в качестве аргументов out 
+                return 0;
+            }
+            else
+            {
+                //x1 = 0; ??? не поняла, как вернуть x1 и x2, если их не существует
+                //x2 = 0; ???
+               return -1;
+            }
+        }
+    }
+}
---
+++ CalculateSquareRoots/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateSquareRoots
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                nachalo:
+                Console.WriteLine("Введите коэффициенты квадратного уравнения (a, b, c):");
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                double c = double.Parse(Console.ReadLine());
+                if (a == 0)
+                {
+                    Console.WriteLine("Коэффициент а не может быть равен нулю!");
+                    goto nachalo;
+                }
+                double x1 = 0; // можно не присваивать значения, если метод с аргументом out
+                double x2 = 0;
+                double res = Operation.CalculateSquareRoots(a, b, c, ref x1, ref x2); 
+                // double res = Operation.CalculateSquareRoots(a, b, c, out x1, out x2);
+                if (res == 1)
+                {
+                    Console.WriteLine("Корни уравнения с коэффициентами a = {0}, b = {1}, c = {2} равны: x1 = " + $"{x1:.#}" + ", x2 = " + $"{x2:.#}", a, b, c);
+                }
+                else if (res == 0)
+                {
+                    Console.WriteLine("Корень уравнения с коэффициентами a = {0}, b = {1}, c = {2} один: x1 = x2 = " + $"{x1:.#}.", a, b, c);
+                }
+                else
+                {
+                    Console.WriteLine("Корней уравнения с коэффициентами a = {0}, b = {1}, c = {2} нет.", a, b, c);
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверный формат введенных данных");
+            }
+            catch (Exception caught)
+            {
+                Console.WriteLine(caught.Message);
+            }
+            
+        }
+    }
+}
