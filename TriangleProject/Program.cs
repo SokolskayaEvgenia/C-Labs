3ac92edbdf8880b31ed5773e5c75e4de97af28bc
Евгения Сокольская <73002756+SokolskayaEvgenia@users.noreply.github.com>
2020-10-29 04:39:32 +0300

    контрольное упражнение 6

---
+++ TriangleProject/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TriangleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Triangle ABC = NewTriangle();
+                Print(ABC);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введены данные неверного формата");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+        static Triangle NewTriangle()
+        {
+            Triangle Tr = new Triangle();
+            Console.WriteLine("Введите значения сторон треугольника: ");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            if (a <= 0 | b <= 0 | c <= 0)
+                throw new Exception("Значение стороны треугольника не может быть меньше или равно 0!");
+            else if (!((a + b) > c & (a + c) > b & (b + c) > a))
+                throw new Exception("Треугольник не существует!");
+            Tr.GetTriangle(a, b, c);
+            return Tr;
+        }
+
+        static void Print(Triangle abc)
+        {
+            Console.WriteLine("Треугольник: сторона 1 = " + $"{abc.Side1():.#}" + ", сторона 2 = " + $"{abc.Side2():.#}" + ", сторона 3 = " + $"{abc.Side3():.#}");
+            Console.WriteLine("Периметр треугольника = " + $"{abc.GetPerimetr():.#}");
+            Console.WriteLine("Площадь треугольника  = " + $"{abc.GetSquare():.#}");
+
+        }
+
+
+    }
+}
---
+++ TriangleProject/Triangle.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+
+class Triangle
+{
+    private double side1;
+    private double side2;
+    private double side3;
+
+    public double Side1()
+    {
+        return side1;
+    }
+
+    public double Side2()
+    {
+        return side2;
+    }
+
+    public double Side3()
+    {
+        return side3;
+    }
+
+    public void GetTriangle(double a, double b, double c)
+    {
+        side1 = a;
+        side2 = b;
+        side3 = c;
+    }
+
+    public double GetPerimetr()
+    {
+        double Perimetr = side1 + side2 + side3;
+        return Perimetr;
+    }
+
+    public double GetSquare()
+    {
+        double P = GetPerimetr() / 2;
+        double Square = Math.Sqrt(P * (P - side1) * (P - side2) * (P - side3));
+        return Square;
+    }
+
+   
+}
