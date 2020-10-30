c057383524fa044eb6314c4a14aeca0e124b69f2
Евгения Сокольская <73002756+SokolskayaEvgenia@users.noreply.github.com>
2020-10-30 23:04:12 +0300

    контрольное упражнение 8

--- TriangleProject/Program.cs
+++ TriangleProject/Program.cs
@@ -8,8 +8,17 @@ namespace TriangleProject
         {
             try
             {
-                Triangle ABC = NewTriangle();
-                Print(ABC);
+                Triangle TR1, TR2, TR3;
+                TR1 = new Triangle(10, 12, 14);
+                TR2 = new Triangle(10, 12);
+                TR3 = new Triangle(10);
+                CheckTriangle(TR1);
+                CheckTriangle(TR2);
+                CheckTriangle(TR3);
+                Print(TR1);
+                Print(TR2);
+                Print(TR3);
+
             }
             catch (FormatException)
             {
@@ -22,27 +31,46 @@ namespace TriangleProject
         }
 
 
-        static Triangle NewTriangle()
-        {
-            Triangle Tr = new Triangle();
-            Console.WriteLine("Введите значения сторон треугольника: ");
-            double a = double.Parse(Console.ReadLine());
-            double b = double.Parse(Console.ReadLine());
-            double c = double.Parse(Console.ReadLine());
-            if (a <= 0 | b <= 0 | c <= 0)
-                throw new Exception("Значение стороны треугольника не может быть меньше или равно 0!");
-            else if (!((a + b) > c & (a + c) > b & (b + c) > a))
-                throw new Exception("Треугольник не существует!");
-            Tr.GetTriangle(a, b, c);
-            return Tr;
-        }
+        //static Triangle NewTriangle()
+        //{
+        //    Triangle Tr = new Triangle();
+        //    Console.WriteLine("Введите значения сторон треугольника: ");
+        //    double a = double.Parse(Console.ReadLine());
+        //    double b = double.Parse(Console.ReadLine());
+        //    double c = double.Parse(Console.ReadLine());
+        //    if (a <= 0 | b <= 0 | c <= 0)
+        //        throw new Exception("Значение стороны треугольника не может быть меньше или равно 0!");
+        //    else if (!((a + b) > c & (a + c) > b & (b + c) > a))
+        //        throw new Exception("Треугольник не существует!");
+        //    Tr.GetTriangle(a, b, c);
+        //    return Tr;
+        //}
 
         static void Print(Triangle abc)
         {
-            Console.WriteLine("Треугольник: сторона 1 = " + $"{abc.Side1():.#}" + ", сторона 2 = " + $"{abc.Side2():.#}" + ", сторона 3 = " + $"{abc.Side3():.#}");
+            if (abc.Side1() == abc.Side2() && abc.Side2() == abc.Side3())
+            {
+                Console.WriteLine("\nРавносторонний треугольник: \nстороны = " + $"{abc.Side1():.#}");
+            }
+            else if (abc.Side1() == abc.Side2())
+            {
+                Console.WriteLine("\nРавнобедренный треугольник: \nстороны 1, 2 = " + $"{abc.Side1():.#}" + ", сторона 3  = " + $"{abc.Side3():.#}");
+            }
+            else
+            {
+                Console.WriteLine("\nТреугольник: \nсторона 1 = " + $"{abc.Side1():.#}" + ", сторона 2 = " + $"{abc.Side2():.#}" + ", сторона 3 = " + $"{abc.Side3():.#}");
+            }
             Console.WriteLine("Периметр треугольника = " + $"{abc.GetPerimetr():.#}");
             Console.WriteLine("Площадь треугольника  = " + $"{abc.GetSquare():.#}");
 
+        }
+
+        public static void CheckTriangle(Triangle abc)
+        {
+            if (abc.Side1() <= 0 | abc.Side2() <= 0 | abc.Side3() <= 0)
+                throw new Exception("Значение стороны треугольника не может быть меньше или равно 0!");
+            else if (!((abc.Side1() + abc.Side2()) > abc.Side3() & (abc.Side1() + abc.Side3()) > abc.Side2() & (abc.Side2() + abc.Side3()) > abc.Side1()))
+                throw new Exception("Треугольник не существует!");
         }
 
 
--- TriangleProject/Triangle.cs
+++ TriangleProject/Triangle.cs
@@ -22,6 +22,35 @@ class Triangle
         return side3;
     }
 
+    public Triangle ()
+    {
+        side1 = 1;
+        side2 = 1;
+        side3 = 1;
+    }
+
+    public Triangle(double a, double b, double c)
+    {
+        side1 = a;
+        side2 = b;
+        side3 = c;
+    }
+
+    public Triangle (double a) //конструктор равностороннего треугольника
+    {
+        side1 = a;
+        side2 = a;
+        side3 = a;
+    }
+
+    public Triangle (double a, double b) //конструктор равнобедренного треугольника
+    {
+        side1 = a;
+        side2 =side1;
+        side3 = b;
+    }
+
+
     public void GetTriangle(double a, double b, double c)
     {
         side1 = a;
@@ -41,6 +70,8 @@ class Triangle
         double Square = Math.Sqrt(P * (P - side1) * (P - side2) * (P - side3));
         return Square;
     }
+
+    
 
    
 }
