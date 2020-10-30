ed3bf2851824c0d9c87b0f5d6ccd3ef570d89acc
Евгения Сокольская <73002756+SokolskayaEvgenia@users.noreply.github.com>
2020-10-30 23:01:53 +0300

    Update Program.cs

--- TriangleCalc/Program.cs
+++ TriangleCalc/Program.cs
@@ -1,78 +1,72 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace TriangleProject
+namespace TriangleCalc
 {
+   
+ 
     class Program
     {
         static void Main(string[] args)
         {
             try
             {
-                Triangle TR1, TR2, TR3;
-                TR1 = new Triangle(10, 12, 14);
-                TR2 = new Triangle(10, 12);
-                TR3 = new Triangle(10);
-                CheckTriangle(TR1);
-                CheckTriangle(TR2);
-                CheckTriangle(TR3);
-                Print(TR1);
-                Print(TR2);
-                Print(TR3);
+               
+                string answer;
+                Console.WriteLine("Выберите тип треугольника: равносторонний? (y/n)");
+                answer = Console.ReadLine();
+                if (answer == "y")
+                {
+                    try
+                    {
+                        Console.WriteLine("Введите значение стороны треугольника:");
+                        double st = double.Parse(Console.ReadLine());
+                        if (st <= 0)
+                            throw new Exception("Значение стороны треугольника не может быть меньше или равно 0!");
+                        double Sq = Operation.TriangleSquare(st);
+                        Console.WriteLine("Площадь треугольника = " + $"{Sq:.#}");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        Console.WriteLine("Введите значения сторон треугольника:");
+                        double x = double.Parse(Console.ReadLine());
+                        double y = double.Parse(Console.ReadLine());
+                        double z = double.Parse(Console.ReadLine());
+                        if (x <= 0 | y <= 0 | z <= 0 )
+                            throw new Exception("Значение стороны треугольника не может быть меньше или равно 0!");
+                        double Sq = Operation.TriangleSquare(x, y, z);
+                        if (Sq != 0)
+                            Console.WriteLine("Площадь треугольника = " + $"{Sq:.#}");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
 
             }
-            catch (FormatException)
+            catch (FormatException caught)
             {
-                Console.WriteLine("Введены данные неверного формата");
+                Console.WriteLine("Возникло исключение! {0}", caught.Message);
             }
-            catch (Exception e)
+            catch (Exception caught)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine("Возникло исключение! {0}", caught.Message);
             }
-        }
-
-
-        //static Triangle NewTriangle()
-        //{
-        //    Triangle Tr = new Triangle();
-        //    Console.WriteLine("Введите значения сторон треугольника: ");
-        //    double a = double.Parse(Console.ReadLine());
-        //    double b = double.Parse(Console.ReadLine());
-        //    double c = double.Parse(Console.ReadLine());
-        //    if (a <= 0 | b <= 0 | c <= 0)
-        //        throw new Exception("Значение стороны треугольника не может быть меньше или равно 0!");
-        //    else if (!((a + b) > c & (a + c) > b & (b + c) > a))
-        //        throw new Exception("Треугольник не существует!");
-        //    Tr.GetTriangle(a, b, c);
-        //    return Tr;
-        //}
-
-        static void Print(Triangle abc)
-        {
-            if (abc.Side1() == abc.Side2() && abc.Side2() == abc.Side3())
-            {
-                Console.WriteLine("\nРавносторонний треугольник: \nстороны = " + $"{abc.Side1():.#}");
-            }
-            else if (abc.Side1() == abc.Side2())
-            {
-                Console.WriteLine("\nРавнобедренный треугольник: \nстороны 1, 2 = " + $"{abc.Side1():.#}" + ", сторона 3  = " + $"{abc.Side3():.#}");
-            }
-            else
-            {
-                Console.WriteLine("\nТреугольник: \nсторона 1 = " + $"{abc.Side1():.#}" + ", сторона 2 = " + $"{abc.Side2():.#}" + ", сторона 3 = " + $"{abc.Side3():.#}");
-            }
-            Console.WriteLine("Периметр треугольника = " + $"{abc.GetPerimetr():.#}");
-            Console.WriteLine("Площадь треугольника  = " + $"{abc.GetSquare():.#}");
+            
 
         }
-
-        public static void CheckTriangle(Triangle abc)
-        {
-            if (abc.Side1() <= 0 | abc.Side2() <= 0 | abc.Side3() <= 0)
-                throw new Exception("Значение стороны треугольника не может быть меньше или равно 0!");
-            else if (!((abc.Side1() + abc.Side2()) > abc.Side3() & (abc.Side1() + abc.Side3()) > abc.Side2() & (abc.Side2() + abc.Side3()) > abc.Side1()))
-                throw new Exception("Треугольник не существует!");
-        }
-
-
     }
 }
