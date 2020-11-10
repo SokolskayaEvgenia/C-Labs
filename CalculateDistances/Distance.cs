741bd6a35e708aad62190c29ce8810bd4c8b2592
Евгения Сокольская <73002756+SokolskayaEvgenia@users.noreply.github.com>
2020-11-10 20:16:26 +0300

    контрольное упражнение 11

---
+++ CalculateDistances/Distance.cs
@@ -0,0 +1,90 @@
+﻿namespace CalculateDistances
+{
+    class Distance
+    {
+        private int feet;
+        private double inches;
+
+        public Distance()
+        { }
+
+        public Distance(int feet, double inches)
+        {
+            this.feet = feet;
+            this.inches = inches;
+        }
+
+        public static Distance operator +(Distance D1, Distance D2)
+        {
+            int newFeet = D1.feet + D2.feet;
+            double newInches = D1.inches + D2.inches;
+            newFeet += (int)(newInches / 12);
+            newInches %= 12;
+            return new Distance(newFeet, newInches);
+        }
+
+        public static Distance operator -(Distance D1, Distance D2)
+        {
+            int newFeet = D1.feet - D2.feet;
+            double newInches = D1.inches - D2.inches;
+            return new Distance(newFeet, newInches);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}\'-{1}\"", feet, inches);
+        }
+
+        private double TotalInches()
+        {
+            return feet * 12 + inches;
+        }
+
+        private double Compare(Distance other)
+        {
+            double d1 = TotalInches();
+            double d2 = other.TotalInches();
+
+            int result;
+            if (d1 < d2)
+                result = -1;
+            else if (d1 > d2)
+                result = +1;
+            else
+                result = 0;
+
+            return result;
+        }
+
+        public static bool operator ==(Distance d1, Distance d2)
+        {
+            return d1.Compare(d2) == 0;
+        }
+
+        public static bool operator !=(Distance d1, Distance d2)
+        {
+            return d1.Compare(d2) != 0;
+        }
+
+        public static bool operator <(Distance d1, Distance d2)
+        {
+            return d1.Compare(d2) < 0;
+        }
+
+        public static bool operator >(Distance d1, Distance d2)
+        {
+            return d1.Compare(d2) > 0;
+        }
+
+        public static bool operator <=(Distance d1, Distance d2)
+        {
+            return d1.Compare(d2) <= 0;
+        }
+
+        public static bool operator >=(Distance d1, Distance d2)
+        {
+            return d1.Compare(d2) >= 0;
+        }
+        
+    }
+}
---
+++ CalculateDistances/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CalculateDistances
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Distance d1 = new Distance(15, 10);
+            Distance d2 = new Distance(15, 10);
+            Distance d3;
+            Distance d4;
+            d3 = d1 + d2;
+            d4 = d1 - d2;
+            Console.WriteLine("Расстояние d1 = " + d1.ToString());
+            Console.WriteLine("Расстояние d2 = " + d2.ToString());
+            Console.WriteLine("d1 + d2 = " + d3.ToString());
+            Console.WriteLine("d1 - d2 = " + d4.ToString());
+            if (d1==d2)
+                Console.WriteLine("расстояния равны");
+            else
+                if (d1> d2)
+                Console.WriteLine("d1 больше, чем d2");
+                else
+                Console.WriteLine("d2 больше, чем d1");
+            
+            
+        }
+    }
+}
